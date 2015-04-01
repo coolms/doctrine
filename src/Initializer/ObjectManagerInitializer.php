ec2d5bce41f3092bcb3a59b8758fbc11cf1fc4c0
Dmitry Popov <d.popov@altgraphic.com>
2015-04-01 10:17:34 -0300

    Initial commit

--- src/Initializer/ObjectManagerInitializer.php
+++ src/Initializer/ObjectManagerInitializer.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * CoolMS2 Doctrine module (http://www.coolms.com/)
+ * CoolMS2 Doctrine Extentions Library (http://www.coolms.com/)
  *
- * @link      http://github.com/coolms/CmsDoctrine for the canonical source repository
+ * @link      http://github.com/coolms/doctrine for the canonical source repository
  * @copyright Copyright (c) 2006-2014 Altgraphic, ALC (http://www.altgraphic.com)
  * @license   http://www.coolms.com/license/new-bsd New BSD License
  * @author    Dmitry Popov <d.popov@altgraphic.com>
