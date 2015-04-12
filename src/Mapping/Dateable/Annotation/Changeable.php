818fac1054a183af6b33a3f74b6a4cd4099434e7
Dmitry Popov <d.popov@altgraphic.com>
2015-04-12 10:58:47 +0300

    Initial commit

--- src/Mapping/Dateable/Annotation/Changeable.php
+++ src/Mapping/Dateable/Annotation/Changeable.php
@@ -3,7 +3,7 @@
  * CoolMS2 Doctrine Extentions Library (http://www.coolms.com/)
  *
  * @link      http://github.com/coolms/doctrine for the canonical source repository
- * @copyright Copyright (c) 2006-2014 Altgraphic, ALC (http://www.altgraphic.com)
+ * @copyright Copyright (c) 2006-2015 Altgraphic, ALC (http://www.altgraphic.com)
  * @license   http://www.coolms.com/license/new-bsd New BSD License
  * @author    Dmitry Popov <d.popov@altgraphic.com>
  */
@@ -18,7 +18,13 @@ use Doctrine\Common\Annotations\Annotation;
  */
 class Changeable extends Annotation
 {
+    /**
+     * @var string
+     */
     public $field;
 
+    /**
+     * @var string
+     */
     public $value;
 }
--- src/Mapping/Dateable/Mapping/Event/Adapter/ORM.php
+++ src/Mapping/Dateable/Mapping/Event/Adapter/ORM.php
@@ -3,20 +3,20 @@
  * CoolMS2 Doctrine Extentions Library (http://www.coolms.com/)
  *
  * @link      http://github.com/coolms/doctrine for the canonical source repository
- * @copyright Copyright (c) 2006-2014 Altgraphic, ALC (http://www.altgraphic.com)
+ * @copyright Copyright (c) 2006-2015 Altgraphic, ALC (http://www.altgraphic.com)
  * @license   http://www.coolms.com/license/new-bsd New BSD License
  * @author    Dmitry Popov <d.popov@altgraphic.com>
  */
 
 namespace CmsDoctrine\Mapping\Dateable\Mapping\Event\Adapter;
 
-use Gedmo\Mapping\Event\Adapter\ORM as BaseAdapterORM,
+use Gedmo\Mapping\Event\Adapter\ORM as GedmoORMAdapter,
     Gedmo\Timestampable\Mapping\Event\TimestampableAdapter;
 
 /**
- * Doctrine event adapter for ORM adapted for Timestampable behavior
+ * Doctrine event adapter for ORM adapted for Dateable behavior
  */
-final class ORM extends BaseAdapterORM implements TimestampableAdapter
+final class ORM extends GedmoORMAdapter implements TimestampableAdapter
 {
     /**
      * {@inheritDoc}
